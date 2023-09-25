701433a767414040e96aec5cf66add325b23db75
Mateus Gambaro <magambaro@hotmail.com>
2023-09-25 09:57:12 -0300

    feat: add project configs

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: '13122100',
+    database: 'rent_tools_api',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: '13122100',
+    database: 'rent_tools_api',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: '13122100',
+    database: 'rent_tools_api',
+    host: 'localhost',
+    dialect: 'mysql',
+  },
+};
