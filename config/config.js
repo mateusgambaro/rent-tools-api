a429748433342a8e0e1a1572b6dc220b58c47a8a
Mateus Gambaro <magambaro@hotmail.com>
2023-09-25 15:37:34 -0300

    feat: add pg client to use supabase

--- config/config.js
+++ config/config.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
+import dotenv from "dotenv";
+dotenv.config();
 
 module.exports = {
   development: {
-    username: 'root',
-    password: '13122100',
-    database: 'rent_tools_api',
-    host: 'localhost',
-    dialect: 'mysql',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
   },
   test: {
-    username: 'root',
-    password: '13122100',
-    database: 'rent_tools_api',
-    host: 'localhost',
-    dialect: 'mysql',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
   },
   production: {
-    username: 'root',
-    password: '13122100',
-    database: 'rent_tools_api',
-    host: 'localhost',
-    dialect: 'mysql',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
   },
 };
